4b485c99a82f858f49ebd6d705069485c3f7118f
r1tikpatil <ritikpatil566@gmail.com>
2024-04-06 14:00:10 +0530

    setup swagger

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+require("./config/mongoose");
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger.json");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use("*", cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
